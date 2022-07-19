538ae80d2cf2f0c9bd0100df278734fdf39140c8
alhaaan97 <alhaaan97@gmail.com>
2022-07-19 17:16:08 +0500

    validation if bag has capacity

--- src/controllers/cuboids.ts
+++ src/controllers/cuboids.ts
@@ -49,3 +49,30 @@ export const create = async (
   }
   
 };
+
+export const update = async (req: Request, res: Response): Promise<Response> => {
+  const id: Id = req.params.id;
+  const { width, height, depth, bagId } = req.body;
+
+  try {
+    const bag = await Bag.query().findById(bagId).withGraphFetched('cuboids')
+    if (!bag) return res.sendStatus(HttpStatus.NOT_FOUND)
+
+    const volumeOfCuboidsOnBag = bag?.cuboids?.filter((cuboid) => cuboid.id !== id).reduce((accumulator, cuboid) => accumulator += cuboid.width * cuboid.height * cuboid.depth, 0)
+    const volumeCurrentCuboid = width * height * depth;
+    const totalVolume = volumeOfCuboidsOnBag && volumeOfCuboidsOnBag + volumeCurrentCuboid
+
+    if (bag && totalVolume && totalVolume > bag?.volume) return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({ message: "Insufficient Capacity in bag" })
+
+    const cuboid = await Cuboid.query().updateAndFetchById(id, {
+      width,
+      height,
+      depth,
+      bagId
+    })
+    return res.status(HttpStatus.OK).json(cuboid);
+  }
+  catch (error) {
+    return res.sendStatus(500)
+  }
+}
